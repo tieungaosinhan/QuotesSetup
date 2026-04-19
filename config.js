5c5f6e4f47f0339e9f7d305607c1fd0344e59b21
tieungaosinhan <tieumachi@gmail.com>
2026-04-19 11:20:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCJEDi5fa69qTJ0iksDy8UeLS5Lw1wirgo",
+	authDomain: "tieungaosinhan.firebaseapp.com",
+	projectId: "tieungaosinhan",
+	storageBucket: "tieungaosinhan.firebasestorage.app",
+	messagingSenderId: "159414617772",
+	appId: "1:159414617772:web:0a246068fdd8303a4c4109"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+window.db = db;
